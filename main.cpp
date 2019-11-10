6d31ac73b8b97d295c29c47e08604d481e747712
psific <katarzyna.malec03@gmail.com>
2019-11-10 16:01:39 +0100

    nowa zmiana

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <strings>
+#include <cmath>
+#include <cstdlib>
+#include "time.h"
 
 using namespace std;
 
 int main()
 {
+
     cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+
+
     return 0;
 }
